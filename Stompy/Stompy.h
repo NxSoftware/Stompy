e367f0458fe1942181aa275f689f2e60236246b1
Steve Wilford <steve.wilford@vmstech.co.uk>
2015-06-17 12:00:44 +0100

    Initial Commit

---
+++ Stompy/Stompy.h
@@ -0,0 +1,19 @@
+//
+//  Stompy.h
+//  Stompy
+//
+//  Created by Steve Wilford on 17/06/2015.
+//  Copyright (c) 2015 Steve Wilford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stompy.
+FOUNDATION_EXPORT double StompyVersionNumber;
+
+//! Project version string for Stompy.
+FOUNDATION_EXPORT const unsigned char StompyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stompy/PublicHeader.h>
+
+
