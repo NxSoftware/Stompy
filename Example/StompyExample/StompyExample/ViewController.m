e1ab5f9b1fd0bd4f441e4b98e14cf6ea3cd04fa7
Steve Wilford <steve@stevewilford.co.uk>
2015-06-18 19:09:24 +0100

    Added disconnect button to the example.

--- Example/StompyExample/StompyExample/ViewController.m
+++ Example/StompyExample/StompyExample/ViewController.m
@@ -12,6 +12,8 @@
 @interface ViewController () <NXStompClientDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *connectButton;
+@property (weak, nonatomic) IBOutlet UIButton *disconnectButton;
+
 @property (nonatomic, strong) NXStompClient *stomp;
 
 @end
@@ -21,6 +23,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.disconnectButton.enabled = NO;
+    
     NSURL *webSocketURL = [NSURL URLWithString:@"http://localhost:8080/ws/websocket"];
     NXStompSocketRocketTransport *transport = [NXStompSocketRocketTransport transportWithURL:webSocketURL];
     self.stomp = [NXStompClient stompWithTransport:transport];
@@ -32,15 +36,23 @@
     [self.stomp connect];
 }
 
+- (IBAction)disconnect:(id)sender {
+    self.connectButton.enabled = NO;
+    self.disconnectButton.enabled = NO;
+    [self.stomp disconnect];
+}
+
 #pragma mark - Stomp Delegate
 
 - (void)stompClientDidConnect:(NXStompClient *)stompClient {
     NSLog(@"DID CONNECT");
+    self.disconnectButton.enabled = YES;
 }
 
 - (void)stompClient:(NXStompClient *)stompClient didDisconnectWithError:(NSError *)error {
     NSLog(@"DISCONNECTED %@", error);
     self.connectButton.enabled = YES;
+    self.disconnectButton.enabled = NO;
 }
 
 @end
